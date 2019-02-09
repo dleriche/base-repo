c6f84d0827c5defb57ac3aa1bd1c2663f2cc1959
DJ LeRiche <dj.leriche@puppet.com>
2019-02-08 22:42:03 -0600

    edit username in users.pp

--- site/profile/manifests/baseline/windows/files.pp
+++ site/profile/manifests/baseline/windows/files.pp
@@ -8,4 +8,13 @@ class profile::baseline::windows::files {
     content => 'File created to test directory and file creation on Windows',
     require => File['c:/DevOps'],
   }
+  acl { 'c:/DevOps':
+    permissions                => [
+     { identity => 'DevOps', rights => ['read'] },
+     { identity => 'DevUser', rights => ['full'] }
+    ],
+    owner                      => 'DevUser',
+    group                      => 'DevOps',
+    inherit_parent_permissions => false,
+  }
 }
--- site/profile/manifests/baseline/windows/users.pp
+++ site/profile/manifests/baseline/windows/users.pp
@@ -3,10 +3,10 @@ class profile::baseline::windows::users  {
     group { 'DevOps':
     ensure          => present,
     }
-    user { 'DevOps':
+    user { 'DevUser':
     ensure          => present,
     managehome      => true,
-    comment         => 'DevOps User created for On-Boarding',
+    comment         => 'DevUser created for On-Boarding',
     password        => Sensitive('Adm1nistrat0r'),
     groups          => ['DevOps','Users'],
     auth_membership => 'minimum',
