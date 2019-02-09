c2f6848222f292aef38760ee5354800da4a065fd
DJ LeRiche <dj.leriche@puppet.com>
2019-02-09 01:43:32 -0600

    add packages to base

--- site/profile/manifests/baseline/windows.pp
+++ site/profile/manifests/baseline/windows.pp
@@ -2,4 +2,5 @@ class profile::baseline::windows  {
 include profile::baseline::windows::users
 include profile::baseline::windows::files
 include profile::baseline::windows::reg_keys
+include profile::baseline::windows::packages
 }
