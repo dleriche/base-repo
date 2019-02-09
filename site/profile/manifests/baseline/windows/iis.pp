91581fed15cf3ceea7f946d7194cf017cf13db7a
DJ LeRiche <dj.leriche@puppet.com>
2019-02-09 00:24:13 -0600

    edited iis.pp

--- site/profile/manifests/baseline/windows/iis.pp
+++ site/profile/manifests/baseline/windows/iis.pp
@@ -4,4 +4,22 @@ class profile::baseline::windows::iis {
     ensure             => present,
     installsubfeatures => true,
   }
+  # Delete the default website to prevent a port binding conflict.
+  iis_site {'Default Web Site':
+    ensure  => absent,
+    require => Windowsfeature['Web-WebServer'],
+  }
+  iis_site { 'minimal':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\minimal',
+    applicationpool => 'DefaultAppPool',
+    require         => [
+      File['minimal'],
+      Iis_site['Default Web Site']
+    ],
+  }
+  file { 'minimal':
+    ensure => 'directory',
+    path   => 'c:\\inetpub\\minimal',
+  }
 }
