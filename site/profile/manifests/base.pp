a4ebbd848a12654cf3a768523bcadc19722111fc
DJ LeRiche <dj.leriche@puppet.com>
2019-02-08 19:09:24 -0600

    add base profiles for win

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,13 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 
+  case $::kernel {
+    'windows': {
+      include ::profile::baseline::windows
+    }
+    default: {
+      fail('Unsupported Operating System!')
+    }
+  }
+
 }
