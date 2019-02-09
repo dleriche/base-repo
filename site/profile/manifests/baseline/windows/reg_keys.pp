aa1f6bb017986a9f3284259018f608cd972977fb
DJ LeRiche <dj.leriche@puppet.com>
2019-02-08 23:15:42 -0600

    add reg_keys.pp

---
+++ site/profile/manifests/baseline/windows/reg_keys.pp
@@ -0,0 +1,18 @@
+class profile::baseline::windows::reg_keys {
+
+  registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}':
+    ensure => present,
+    type   => dword,
+    data   => '0x00000001',
+  }
+  registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073':
+    ensure => present,
+    type   => dword,
+    data   => '0x00000001',
+  }
+  registry_value { 'HKEY_LOCAL_MACHINE\Software\Microsoft\ Windows\CurrentVersion\Reliability\ShutdownReasonUI':
+    ensure => present,
+    type   => dword,
+    data   => '0x00000001',
+  }
+}
