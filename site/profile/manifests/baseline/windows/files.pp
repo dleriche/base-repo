9be002b4555b087f6d67ff0db50a35459c61b7cc
DJ LeRiche <dj.leriche@puppet.com>
2019-02-08 22:42:17 -0600

    edit files.pp

--- site/profile/manifests/baseline/windows/files.pp
+++ site/profile/manifests/baseline/windows/files.pp
@@ -5,13 +5,12 @@ class profile::baseline::windows::files {
   }
   file { 'c:/DevOps/testfile.txt':
     ensure  => present,
-    content => 'File created to test directory and file creation on Windows',
-    require => File['c:/DevOps'],
+    content => 'File created to test directory and file creation on Windows'
   }
   acl { 'c:/DevOps':
     permissions                => [
-     { identity => 'DevOps', rights => ['read'] },
-     { identity => 'DevUser', rights => ['full'] }
+      { identity => 'DevOps', rights => ['read'] },
+      { identity => 'DevUser', rights => ['full'] }
     ],
     owner                      => 'DevUser',
     group                      => 'DevOps',
