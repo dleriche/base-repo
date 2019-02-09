a7fb543ee98f6faf62144daa0dba6da3724df200
DJ LeRiche <dj.leriche@puppet.com>
2019-02-08 21:04:01 -0600

    edited users.pp

--- site/profile/manifests/baseline/windows/users.pp
+++ site/profile/manifests/baseline/windows/users.pp
@@ -1,12 +1,17 @@
 class profile::baseline::windows::users  {
 
   user { 'DevOps':
-    ensure   => present,
-    password => Sensitive('Adm1nistrat0r'),
-    groups   => ['DevOps']
+    ensure          => present,
+    managehome      => true,
+    comment         => 'DevOps User created for On-Boarding',
+    password        => Sensitive('Adm1nistrat0r'),
+    groups          => ['DevOps','Users'],
+    auth_membership => 'minimum',
   }
   group { 'DevOps':
-    ensure => present,
-    members => ['DevOps'],
+    ensure          => present,
+    comment         => 'DevOps Group created for On-Boarding',
+    members         => ['DevOps'],
+    auth_membership => false,
     }
 }
