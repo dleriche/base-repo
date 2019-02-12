8ce873a6a8a598f5e81312bc681357e849b3a691
DJ LeRiche <dj.leriche@puppet.com>
2019-02-11 16:29:48 -0800

    clean up formatting windows.pp

--- site/profile/manifests/baseline/windows.pp
+++ site/profile/manifests/baseline/windows.pp
@@ -1,6 +1,6 @@
 class profile::baseline::windows  {
-include profile::baseline::windows::users
-include profile::baseline::windows::files
-include profile::baseline::windows::reg_keys
-include profile::baseline::windows::packages
+  include profile::baseline::windows::users
+  include profile::baseline::windows::files
+  include profile::baseline::windows::reg_keys
+  include profile::baseline::windows::packages
 }
